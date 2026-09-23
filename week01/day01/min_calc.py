7133690f3063f5dbe1b1c40c65a7b376c1678009
ShalinoiViolet <yaroslav.golomazov@gmail.com>
2026-09-23 12:58:42 +0700

    Add basic calculator functionality in min_calc.py

---
+++ week01/day01/min_calc.py
@@ -0,0 +1,11 @@
+a = int(input('Первое число: '))
+b = int(input('Второе число: '))
+c = input('Операция: ')
+if c == '*':
+    print(a * b)
+elif c == '+':
+    print(a + b)
+elif c == '-':
+    print(a - b)
+elif c == '/':
+    print(a / b)
