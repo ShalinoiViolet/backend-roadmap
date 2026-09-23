901e8e00f035b5c8c7ace2f3d6ffafcc733dbb4d
ShalinoiViolet <yaroslav.golomazov@gmail.com>
2026-09-23 12:45:17 +0700

    Add day 01 Python exercises

---
+++ week01/day01/prog1.py
@@ -0,0 +1,10 @@
+name = 'Violet'
+age = 18
+city = 'Moscow'
+Spec = 'Python backend'
+game = 'Assassins Creed 2'
+print('Имя:', name)
+print('Возраст:', age)  
+print('Город:', city)
+print('Специализация:', Spec)
+print('Любимая игра:', game)
